34e8bb0e6b424b5b74e2e6a6f0b291903b49638a
justinCplat <justin@cplat.io>
2025-02-12 11:22:03 +0900

    [style] feat: 기본 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,37 @@ const Home = () => {
   const [inputText, setInputText] = useState<string>("");
   return (
     <div className="flex flex-col w-screen h-[2000px] max-w-[800px] mx-auto pt-[60px] relative">
-      <header className="relative sticky top-0 py-[16px] bg-white">
-        <textarea
-          className="w-full border-2 resize-none pr-[48px]"
-          value={inputText}
-          onChange={(e) => {
-            setInputText(e.target.value);
-          }}
-        ></textarea>
-        <button className="absolute right-0 top-1/2 transform -translate-y-1/2">
-          검색
-        </button>
+      <header className="relative sticky top-0 bg-white border-2">
+        <div>
+          <textarea
+            className="w-full resize-none pr-[48px] border-b-2 border-black"
+            value={inputText}
+            onChange={(e) => {
+              setInputText(e.target.value);
+            }}
+          ></textarea>
+          <div className="absolute right-0 top-1/2 transform -translate-y-1/2 flex items-center gap-[10px]">
+            {inputText && (
+              <button className="" onClick={() => setInputText("")}>
+                지우기
+              </button>
+            )}
+            <button className="">검색</button>
+          </div>
+        </div>
+        <div className="flex gap-[10px] items-center">
+          <button>파일</button>
+          <button>음성</button>
+        </div>
       </header>
-      <main className="flex flex-grow bg-green-50">{inputText}</main>
-      <footer className="bg-blue-50 sticky bottom-0">footer</footer>
+      <main className="flex flex-grow bg-green-50 flex-col justify-between">
+        {Array.from({ length: 10 }, (_, i) => i + 1).map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+      </main>
+      <footer className="bg-blue-50 sticky bottom-0 border-2 h-[50px]">
+        footer
+      </footer>
     </div>
   );
 };
