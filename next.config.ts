e661d973cf5744c753f061427e6d63dd6de952dd
justinCplat <justin@cplat.io>
2025-02-12 15:28:53 +0900

    [deploy] feat: github pages 배포 로직 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  assetPrefix: "https://cplat-io.github.io/ai-agent-front/",
 };
 
 export default nextConfig;
